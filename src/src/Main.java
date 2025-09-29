d9493656c4ae2a0a3ad8fcd4f196f7383b34110f
Rohini881-ui <rohinishreet@gmail.com>
2025-09-29 06:44:36 -0700

    Create Main.java

---
+++ src/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🏏 Cricket Score Management System Started!");
+
+        // Example player
+        Player p1 = new Player("Rohini");
+        p1.displayStats();
+    }
+}
